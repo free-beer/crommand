c845cecde509ce14114a14f088d00c7e70a54172
Peter Wood <pw0470@gmail.com>
2023-06-03 13:49:27 +0100

    Reworked the exception classes.

--- src/exceptions.cr
+++ src/exceptions.cr
@@ -1,9 +1,9 @@
 module Crommand
   # A base type for the exceptions that the Crommand library can generate.
-  class CrommandException < ::Exception
+  class Exception < ::Exception
   end
 
   # An exception type used by the library optional functionality.
-  class OptionalError < CrommandException
+  class OptionalError < Crommand::Exception
   end
 end
